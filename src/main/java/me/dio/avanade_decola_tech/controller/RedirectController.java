de8d64ec6d4b2572e1acb9f76a1425315c022239
Ana Paula Dantas <dantaspereiraana@gmail.com>
2025-03-05 19:14:51 -0300

    Swagger Class

---
+++ src/main/java/me/dio/avanade_decola_tech/controller/RedirectController.java
@@ -0,0 +1,15 @@
+package me.dio.avanade_decola_tech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui.html");
+    }
+}
+
